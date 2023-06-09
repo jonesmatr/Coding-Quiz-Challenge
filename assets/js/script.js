3916511b951c83822be19af5386e12d733e4fced
jonesmatr <32439889+jonesmatr@users.noreply.github.com>
2023-06-09 15:44:52 -0400

    Added screen at the end of game that displays user's score and provides form field for initials. After user clicks submit, they see a button for going back to the beginning of the game and one for clearing the high scores.

--- assets/js/script.js
+++ assets/js/script.js
@@ -168,7 +168,62 @@ function endGame() {
   questionContainer.style.display = "none";
   choices.style.display = "none";
   resultElement.textContent = "Quiz completed!";
-  nextButton.style.display = "none";
+
+
+var endScreen = document.getElementById("endScreen");
+endScreen.style.display = "block";
+
+var scoreElement = document.getElementById("score");
+scoreElement.textContent = "Your score: " + score;
+
+var initialsForm = document.getElementById("initialsForm");
+initialsForm.addEventListener("submit", saveHighScore);
 }
 
+function saveHighScore(event) {
+    event.preventDefault();
+
+    var initialsInput = document.getElementById("initialsInput");
+    var initials = initialsInput.value.trim();
+
+    if (initials !== "") {
+        var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+        highScores.push({ initials: initials, score: score });
+        highScores.sort((a,b) => b.score - a.score);
+        localStorage.setItem("highScores", JSON.stringify(highScores));
+        showHighScores();
+    }
+}
+
+function showHighScores() {
+    var highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
+    var highScoresScreen = document.getElementById("highScoresScreen");
+    highScoresScreen.style.display = "block";
+
+    var highScoresList = document.getElementById("highScoresList");
+    highScoresList.innerHTML = "";
+
+    for (var i = 0; i < highScores.length; i++) {
+        var listItem = document.createElement("li");
+        listItem.textContent = highScores[i].initials + ": " + highScores[i].score;
+        highScoresList.appendChild(listItem);
+    }
+}
+
+var goBackButton = document.getElementById("goBackButton");
+goBackButton.addEventListener("click", goBack);
+
+var clearScoresButton = document.getElementById("clearScoresButton");
+clearScoresButton.addEventListener("click", clearHighScores);
+
+function goBack() {
+    location.reload();
+}
+
+function clearHighScores() {
+    localStorage.removeItem("highScores");
+    var highScoresList = document.getElementById("highScoresList");
+    highScoresList.innerHTML = "";
+}
 //Add form field for user to enter initials and save score to local storage. Rank scores in order of highest to lowest.
